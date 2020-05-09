3eb7857f532b185b87c33285a7dcc7b95ea0d2cd
Mo Dongsong <modongsong@126.com>
2020-05-09 15:18:45 +0800

    Updated:

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ai-transformersx',
-    version='0.1.0',
+    version='0.1.1',
     description='A project for downloading pretrained model of huggingface easily',
     long_description=long_description,
     long_description_content_type='text/markdown',
