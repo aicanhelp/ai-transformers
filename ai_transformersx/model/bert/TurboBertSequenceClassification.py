328ec03d2d59f68c378677cfe292bf7bccdf55f1
Mo Dongsong <modongsong@126.com>
2020-06-07 12:56:32 +0800

    Updated:

--- ai_transformersx/model/bert/TurboBertSequenceClassification.py
+++ ai_transformersx/model/bert/TurboBertSequenceClassification.py
@@ -57,6 +57,7 @@ class TurboBertForSequenceClassification(nn.Module):
     @staticmethod
     def from_pretrained(model_id_or_path: str, config, **kwargs):
         torch_model = BertModel.from_pretrained(model_id_or_path, config=config, **kwargs)
+        torch_model.to('cuda')
         turbo_model = turbo_transformers.BertModelWithPooler.from_torch(torch_model)
         model = TurboBertForSequenceClassification(turbo_model, config)
         return model
